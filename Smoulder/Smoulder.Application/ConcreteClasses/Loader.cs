023acb67c1f37f15ef8faf28727409dac7fdaf72
Ben Wesson <Ben.Wesson@3squared.com>
2018-10-26 16:36:12 +0100

    Cleanup after the removal of the test classes

--- Smoulder/Smoulder.Application/ConcreteClasses/Loader.cs
+++ Smoulder/Smoulder.Application/ConcreteClasses/Loader.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Smoulder.Concrete;
 
 namespace Smoulder.Application.ConcreteClasses
 {
--- Smoulder/Smoulder.Tests/TestConcreteClasses/DistributorTest.cs
+++ Smoulder/Smoulder.Tests/TestConcreteClasses/DistributorTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Smoulder.Concrete;
 using Smoulder.Interfaces;
 
 namespace Smoulder.Tests.TestConcreteClasses
