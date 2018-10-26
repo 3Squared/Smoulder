2eb1a8b867b7b92a202ddca008a82d287004f37b
Ben Wesson <Ben.Wesson@3squared.com>
2018-10-26 14:09:11 +0100

    Add concrete objects to test they work as intended

---
+++ Smoulder/Smoulder/Concrete/ConcreteLoader.cs
@@ -0,0 +1,11 @@
+﻿namespace Smoulder.Concrete
+{
+    public class ConcreteLoader : LoaderBase
+    {
+        public override void Action()
+        {
+            var data = new ConcreteProcessorDataObject();
+            ProcessorQueue.Enqueue(data);
+        }
+    }
+}
---
+++ Smoulder/Smoulder/Concrete/ConcreteProcessorDataObject.cs
@@ -0,0 +1,10 @@
+﻿using Smoulder.Interfaces;
+
+namespace Smoulder.Concrete
+{
+    public class ConcreteProcessorDataObject : IProcessDataObject
+    {
+        public int DataValue1 = 1;
+        public int DataValue2 = 2;
+    }
+}
