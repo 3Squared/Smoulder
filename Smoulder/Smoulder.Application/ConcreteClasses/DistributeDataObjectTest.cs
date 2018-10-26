3976c7c3c6695524db146c000b18ed95f819303d
Ben Wesson <Ben.Wesson@3squared.com>
2018-10-26 14:10:12 +0100

    Start hellow world application to test smoulder works

---
+++ Smoulder/Smoulder.Application/ConcreteClasses/DistributeDataObjectTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Smoulder.Interfaces;
+
+namespace Smoulder.Tests.TestConcreteClasses
+{
+    class DistributeDataObjectTest : IDistributeDataObject
+    {
+        public int DataValue1 = 1;
+        public int DataValue2 = 2;
+    }
+}
---
+++ Smoulder/Smoulder.Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Smoulder.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smoulderFactory = new SmoulderFactory();
+            var smoulder = smoulderFactory.Build(new Loader(), new Processor(), new Distributor());
+        }
+    }
+}
