e719e3191a60a14383acacd620f2ee4a3af6c12e
Ben Wesson <Ben.Wesson@3squared.com>
2018-12-13 16:33:44 +0000

    Add changeIdentity message type

---
+++ Smoulder/TrainDataListener/TrainData/TrainChangeIdentityData.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace TrainDataListener.TrainData
+{
+    [XmlRoot(ElementName = "TrainChangeIdentityData", Namespace = "http://xml.networkrail.co.uk/ns/2008/Train")]
+    public class TrainChangeIdentityData
+    {
+        [XmlElement(ElementName = "OriginalTrainID", Namespace = "http://xml.networkrail.co.uk/ns/2008/Train")]
+        public string OriginalTrainID { get; set; }
+
+        [XmlElement(ElementName = "EventTimestamp", Namespace = "http://xml.networkrail.co.uk/ns/2008/Train")]
+        public string EventTimestamp { get; set; }
+
+        [XmlElement(ElementName = "RevisedTrainID", Namespace = "http://xml.networkrail.co.uk/ns/2008/Train")]
+        public string RevisedTrainID { get; set; }
+
+        [XmlElement(ElementName = "TrainServiceCode", Namespace = "http://xml.networkrail.co.uk/ns/2008/Train")]
+        public string TrainServiceCode { get; set; }
+
+        [XmlElement(ElementName = "TrainFileAddress", Namespace = "http://xml.networkrail.co.uk/ns/2008/Train")]
+        public string TrainFileAddress { get; set; }
+    }
+}
