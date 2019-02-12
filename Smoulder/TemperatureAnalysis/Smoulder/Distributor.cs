6cb249b903d6a69f1f432fbde87d1e57934e1f79
Ben Wesson <Ben.Wesson@3squared.com>
2019-02-12 17:41:13 +0000

    Add month handling to distributor

--- Smoulder/TemperatureAnalysis/Smoulder/Distributor.cs
+++ Smoulder/TemperatureAnalysis/Smoulder/Distributor.cs
@@ -19,6 +19,10 @@ namespace TemperatureAnalysis.Smoulder
         private Peak _maxPeak = new Peak();
         private DateTime? _startDate = null;
         private DateTime _endDate = new DateTime();
+        private int? _currentMonth = null;
+        private int? _currentYear = null;
+
+        private List<MonthData> Months = new List<MonthData>();
 
         private List<Peak> peaks = new List<Peak>();
 
@@ -35,9 +39,40 @@ namespace TemperatureAnalysis.Smoulder
                         _startDate = data.Peak.Time.Date;
                     }
 
+                    if (!_currentMonth.HasValue)
+                    {
+                        _currentMonth = data.Peak.Time.Month;
+                        _currentYear = data.Peak.Time.Year;
+                    }
+
                     if (data.Peak.Time.Date > _endDate)
                     {
                         _endDate = data.Peak.Time;
+                    }
+
+                    if (_currentMonth < data.Peak.Time.Month || _currentYear < data.Peak.Time.Year)
+                    {
+                        Months.Add(new MonthData
+                        {
+                            _average = _average,
+                            _averagePeak = _averagePeak,
+                            _endDate = _endDate,
+                            _maxPeak = _maxPeak,
+                            _startDate = _startDate,
+                            _totalCount = _totalCount,
+                            Month = _currentMonth.Value
+                        });
+                        _totalCount = 0;
+                        _peakCount = 0;
+                        _totalSum = 0;
+                        _peakSum = 0;
+                        _average = 0;
+                        _averagePeak = 0;
+                         _maxPeak = new Peak();
+                        _startDate = null;
+                        _endDate = new DateTime();
+                        _currentMonth = data.Peak.Time.Month;
+                        _currentYear = data.Peak.Time.Year;
                     }
 
 
@@ -71,14 +106,38 @@ namespace TemperatureAnalysis.Smoulder
 
         public override async Task Finalise()
         {
-            Console.WriteLine("Final Results:");
-            Console.WriteLine("Average Temperature during office hours {0:0.0}", _average);
-            Console.WriteLine("Average Peak Temperature during office hours {0:0.0}", _averagePeak);
-            Console.WriteLine("Max Temperature {0:0.0}", _maxPeak.Temperature);
-            Console.WriteLine("Hottest Day {0}",_maxPeak.Time.ToShortDateString());
-            Console.WriteLine("Number of Data points {0}",_totalCount);
-            Console.WriteLine("Start Date {0}", _startDate?.ToShortDateString());
-            Console.WriteLine("End Date {0}", _endDate.ToShortDateString());
+            Months.Add(new MonthData
+            {
+                _average = _average,
+                _averagePeak = _averagePeak,
+                _endDate = _endDate,
+                _maxPeak = _maxPeak,
+                _startDate = _startDate,
+                _totalCount = _totalCount,
+                Month = _currentMonth.Value
+            });
+            foreach (var monthData in Months)
+            {
+                Console.WriteLine("For Month {0}",monthData.Month);
+                Console.WriteLine("Average Temperature during office hours {0:0.0}", monthData._average);
+                Console.WriteLine("Average Peak Temperature during office hours {0:0.0}", monthData._averagePeak);
+                Console.WriteLine("Max Temperature {0:0.0}", monthData._maxPeak.Temperature);
+                Console.WriteLine("Hottest Day {0}", monthData._maxPeak.Time.ToShortDateString());
+                Console.WriteLine("Number of Data points {0}", monthData._totalCount);
+                Console.WriteLine("Start Date {0}", monthData._startDate?.ToShortDateString());
+                Console.WriteLine("End Date {0}", monthData._endDate.ToShortDateString());
+            }
+        }
+
+        private class MonthData
+        {
+            public int Month = 0;
+            public decimal _average;
+            public decimal _averagePeak;
+            public Peak _maxPeak = new Peak();
+            public int _totalCount;
+            public DateTime? _startDate = null;
+            public DateTime _endDate = new DateTime();
         }
     }
 }
