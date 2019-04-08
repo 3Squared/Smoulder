96a6b483cba5bcd63bb311bfe589e614dafb518a
Ben Wesson <Ben.Wesson@3squared.com>
2019-04-08 15:08:18 +0100

    Add updated data

--- Smoulder/TemperatureAnalysis/Program.cs
+++ Smoulder/TemperatureAnalysis/Program.cs
@@ -13,7 +13,7 @@ namespace TemperatureAnalysis
             var smoulder = smoulderFactory.Build(new Loader(), new Processor(), new Distributor());
             smoulder.Start().Wait();
 
-            Thread.Sleep(10000);
+            Thread.Sleep(30000);
 
             smoulder.Stop().Wait();
 
